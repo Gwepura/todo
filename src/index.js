3b0b8d6b305a9bc08421868712feffec21d5dc9e
Gwepura <aketani32@gmail.com>
2020-03-10 16:19:28 +0200

    Step 2 - Serve the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const api = express();
+api.use(express.static(__dirname + '/public'));
+
 api.listen(3000, () => {
   console.log('API up and running!');
 });
 
-api.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
+// api.get('/', (req, res) => {
+//   res.send('Hello, world!');
+// });
